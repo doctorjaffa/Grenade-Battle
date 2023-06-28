22dab35d5cfca06751c8abb9d5c6ee2fb9384028
doctorjaffa <nathanharris1502@gmail.com>
2023-06-28 12:46:43 +0100

    Fix easing function to use ease out

--- Grenade Battle VS/Easing.cpp
+++ Grenade Battle VS/Easing.cpp
@@ -2,5 +2,7 @@
 
 sf::Vector2f Easing::EaseOutQuad(sf::Vector2f begin, sf::Vector2f change, float duration, float time)
 {
-    return (change / (duration * duration)) * time * (2 - time) + begin;
+    //return (change / (duration * duration)) * time * (2 - time) + begin;
+
+    return -((change * time * time) / duration * duration) + (2.0f * ((change * time) / duration)) + begin;
 }
